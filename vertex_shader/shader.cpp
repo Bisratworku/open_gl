35b65a5bbc1817e44a1dc70209446b1b080be2e5
Bisratworku <101055036+Bisratworku@users.noreply.github.com>
2024-08-11 09:24:07 -0700

    Recangle Created

--- vertex_shader/shader.cpp
+++ vertex_shader/shader.cpp
@@ -92,7 +92,11 @@ int main() {
 	unsigned int VBO, VAO;
 	glGenBuffers(1, &VBO);
 	glGenVertexArrays(1, &VAO);
+	
+	
 	glBindVertexArray(VAO);
+	
+	
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexes), vertexes, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -114,8 +118,7 @@ int main() {
 		glfwSwapBuffers(window);
 	
 	}
-
-
+	
 
 	glfwTerminate();
 	return 0;
